e9d12f5b65b103282b42ffdda52a9561e27b7657
Giang Chúc <chucgiang218@gmail.com>
2024-06-19 19:24:13 +0700

    Add project files.

---
+++ Số lẻ nhỏ hơn n.cpp
@@ -0,0 +1,17 @@
+﻿// Số lẻ nhỏ hơn n.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout << "Nhap x\n";
+	cin >> x;
+	cout << "Ket qua la:";
+	for (int i = 0;i < x;i++)
+	{
+		if (i % 2 != 0)
+			cout << i <<" ";
+	}
+}
